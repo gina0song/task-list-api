791b037441b1579ccadf3f846dd9d665670b521e
Gina Song <gina0song@gmail.com>
2025-11-01 14:59:47 -0700

    Implement basic Read/Create (POST, GET) routes for Task resource

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,64 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, request,abort, jsonify
+from ..models.task import Task
+from ..db import db
+
+tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks') 
+
+# Route Helper Method
+def validate_task(id):
+    try:
+        id = int(id)
+    except ValueError:
+        invalid = {"message": f"Task id ({id}) is invalid."}
+        abort(make_response(invalid, 400))
+
+    query = db.select(Task).where(Task.id == id)
+    task = db.session.scalar(query)
+    if not task:
+        not_found = {"message": f"Task with id ({id}) not found."}
+        abort(make_response(not_found, 404))
+
+    return task
+
+
+# CRUD Routes
+@tasks_bp.post("")
+def create_task():
+    # (try...except block) necessary to catch the KeyError when title or description is missing. 
+    try:
+        request_body = request.get_json()
+
+        new_task = Task.from_dict(request_body)
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        task_response = new_task.to_dict()
+
+        return jsonify(task_response), 201
+    
+    except KeyError as e:
+        error_message = f"Bad Request: {e} is required."
+        return jsonify({"message": error_message}), 400
+
+    # Catches other errors
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return jsonify({"message": "Internal Server Error"}), 500
+
+@tasks_bp.get("")
+def get_tasks():
+
+    query = db.select(Task)
+    all_tasks = db.session.scalars(query).all()
+
+    tasks_response = [task.to_dict() for task in all_tasks]
+
+    return jsonify(tasks_response), 200
+
+@tasks_bp.get("/<id>")
+def get_single_task(id):
+    task = validate_task(id)
+
+    return jsonify(task.to_dict()), 200
+
