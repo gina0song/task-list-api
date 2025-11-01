59cf0f7d79d8a19feaa1e5766106c77e301c49b3
Gina Song <gina0song@gmail.com>
2025-11-01 14:05:06 -0700

    Define Model Helper Methods (to_dict/from_dict) for wave_01

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,30 @@
+from typing import Optional
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title:Mapped[str] = mapped_column(nullable=False)
+    description:Mapped[str] = mapped_column(nullable=False)
+    completed_at:Mapped[Optional[datetime]] = mapped_column(nullable=True, default=None)
+
+
+    # Method to convert Task instance to dictionary
+    def to_dict(self) :
+        task_dict = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
+        return task_dict
+
+    # Class method to create Task instance from dictionary
+    @classmethod
+    def from_dict(cls, data: dict) -> "Task":
+        return cls(
+            title=data.get("title"),
+            description=data.get("description"),
+            completed_at=data.get("completed_at")
+        )
