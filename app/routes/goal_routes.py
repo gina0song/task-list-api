a2139a56718424f3d0070aceca7e27096fbfa04b
Gina Song <gina0song@gmail.com>
2025-11-02 22:13:27 -0800

    Using shared utilities to refactor goal_routes and task_routes

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,11 +1,10 @@
-from flask import Blueprint, make_response, request,abort, jsonify
-
-from app.routes.task_routes import validate_task
-from ..models.goal import Goal
-from ..db import db
-from datetime import datetime
 import os
-import requests
+from flask import Blueprint, make_response, request, jsonify
+
+from app import db
+from ..models.goal import Goal
+from .route_utilities import validate_model, create_model, get_all_models, update_model, delete_model
+from .task_routes import validate_task
 
 SLACK_API_URL = "https://slack.com/api/chat.postMessage"
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")     
@@ -13,47 +12,21 @@ SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 goals_bp = Blueprint('goals', __name__, url_prefix='/goals')
 
 # Route Helper Method
-def validate_goal(ID):
-    try:
-        ID = int(ID)
-    except ValueError:
-        invalid = {"message": f"Goal ID ({ID}) is invalid."}
-        abort(make_response(invalid, 400))
-
-    query = db.select(Goal).where(Goal.id == ID)
-    goal = db.session.scalar(query)
-    if not goal:
-        not_found = {"message": f"Goal with ID ({ID}) not found."}
-        abort(make_response(not_found, 404))
-
-    return goal
+def validate_goal(id):
+    return validate_model(Goal,id)
 
 # CRUD Routes
 @goals_bp.post("")
 def create_goal():
-    try:
-        request_body = request.get_json()
+    request_body = request.get_json()
+    goal_dict, status_code = create_model(Goal, request_body)
+    return jsonify(goal_dict), status_code
 
-        new_goal = Goal.from_dict(request_body)
-
-        db.session.add(new_goal)
-        db.session.commit()
-
-        goal_response = new_goal.to_dict()
-
-        return jsonify(goal_response), 201
-    
-    except KeyError:
-        return jsonify({"details": "Invalid data"}), 400
-
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return jsonify({"message": "Internal Server Error"}), 500
-    
 @goals_bp.get("")
 def get_goals():
-    goals = db.session.scalars(db.select(Goal)).all()
-    return jsonify([goal.to_dict() for goal in goals]), 200     
+    goals_list, status_code = get_all_models(Goal)
+    return jsonify(goals_list), status_code
+
 
 @goals_bp.get("/<id>")  
 def get_goal(id):
@@ -63,26 +36,16 @@ def get_goal(id):
 @goals_bp.put("/<id>")
 def update_goal(id):
     goal = validate_goal(id)
-
     request_body = request.get_json()
-
-    if "title" in request_body:
-        goal.title = request_body["title"]
-
-    db.session.commit()
-
+    update_model(goal, request_body, ['title'])
     return "", 204
 
 
 @goals_bp.delete("/<id>")
 def delete_goal(id):
     goal = validate_goal(id)
-
-    db.session.delete(goal)
-    db.session.commit()
-
-    return jsonify({"details": f'Goal {goal.id} "{goal.title}" successfully deleted'}), 204
-
+    response_dict, status_code = delete_model(goal)
+    return jsonify(response_dict), status_code
 
 
 
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, make_response, request,abort, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from ..models.task import Task
 from ..db import db
+from .route_utilities import validate_model, create_model, get_all_models, update_model, delete_model
 from datetime import datetime
 import os
 import requests
+
 
 SLACK_API_URL = "https://slack.com/api/chat.postMessage"
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
@@ -11,44 +13,16 @@ SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks') 
 
 # Route Helper Method
-def validate_task(ID):
-    try:
-        ID = int(ID)
-    except ValueError:
-        invalid = {"message": f"Task ID ({ID}) is invalid."}
-        abort(make_response(invalid, 400))
-
-    query = db.select(Task).where(Task.id == ID)
-    task = db.session.scalar(query)
-    if not task:
-        not_found = {"message": f"Task with ID ({ID}) not found."}
-        abort(make_response(not_found, 404))
-
-    return task
+def validate_task(id):
+    return validate_model(Task, id)
 
 
 # CRUD Routes
 @tasks_bp.post("")
 def create_task():
-    # (try and except block) necessary to catch the KeyError when title or description is missing. 
-    try:
-        request_body = request.get_json()
-
-        new_task = Task.from_dict(request_body)
-
-        db.session.add(new_task)
-        db.session.commit()
-
-        task_response = new_task.to_dict()
-
-        return jsonify(task_response), 201
-    
-    except KeyError:
-        return jsonify({"details": "Invalid data"}), 400
-
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return jsonify({"message": "Internal Server Error"}), 500
+    request_body = request.get_json()
+    task_dict, status_code = create_model(Task, request_body)
+    return jsonify(task_dict), status_code
 
 @tasks_bp.get("")
 def get_tasks():
@@ -58,14 +32,15 @@ def get_tasks():
 
     # Sort tasks if sort parameter is provided,"asc" for ascending and "desc" for descending
     if sort_by == 'asc':
-        all_tasks.sort(key=lambda task: task.title, reverse=False)
+        query = db.select(Task).order_by(Task.title.asc())
     elif sort_by == 'desc':
-        all_tasks.sort(key=lambda task: task.title, reverse=True)
+        query = db.select(Task).order_by(Task.title.desc())
+    else:
+        query = db.select(Task)
+    
+    tasks = db.session.scalars(query).all()
 
-    tasks_response = [task.to_dict() for task in all_tasks]
-
-    return jsonify(tasks_response), 200
-
+    return jsonify([task.to_dict() for task in tasks]), 200
 
 @tasks_bp.get("/<id>")
 def get_single_task(id):
