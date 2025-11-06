bafb8498e62cd68259a20adb1b7e40c04c530fa4
Gina Song <gina0song@gmail.com>
2025-11-05 23:21:34 -0800

    add ( sqlite:///:memory:) in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', "sqlite:///:memory:")
 
     if config:
         app.config.update(config)
